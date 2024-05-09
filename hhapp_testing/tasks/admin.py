eaeeaf70bb4e517fea731e49869a360ca7f8292b
aleks <aleks.ttr@yandex.ru>
2024-05-09 15:06:57 +0300

    Registered in admin-file models: Category, Refrigerator. Added new filed "no_frost" in model Refrigerator.

--- hhapp_testing/tasks/admin.py
+++ hhapp_testing/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Refrigerator
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Refrigerator)
+class RefrigeratorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'no_frost', 'amount_compressor', 'category')
+    list_filter = ('name', 'no_frost', 'amount_compressor', 'category')
+    search_fields = ('name', 'no_frost', 'amount_compressor', 'category')
--- hhapp_testing/tasks/models.py
+++ hhapp_testing/tasks/models.py
@@ -10,9 +10,20 @@ class Category(models.Model):
     id_cat = models.PositiveIntegerField(primary_key=True, serialize=True)
     name = models.CharField(max_length=50, choices=DEV_CATEGORY, db_index=True, blank=False)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name',]
+
 
 class Refrigerator(models.Model):
     id_ref = models.PositiveIntegerField(primary_key=True, serialize=True)
     name = models.CharField(verbose_name='Name', max_length=100, blank=False)
     amount_compressor = models.PositiveSmallIntegerField(blank=False)
+    no_frost = models.BooleanField(blank=False, default=False)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    class Meta:
+        verbose_name = 'Refrigerator'
+        verbose_name_plural = 'Refrigerators'
+        ordering = ['name', 'amount_compressor', 'category']
